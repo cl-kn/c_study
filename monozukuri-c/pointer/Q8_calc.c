08d0aaf9f99e1add06b9b5bdf8f63e9e54f2c151
cl-kn <72800355+cl-kn@users.noreply.github.com>
2020-11-11 09:37:43 +0900

    edit 1111 (at home)

--- monozukuri-c/pointer/Q8_calc.c
+++ monozukuri-c/pointer/Q8_calc.c
@@ -9,5 +9,28 @@
 
 int main(void)
 {
+    char *pnum1;
+    short *pnum2;
+    long *pnum3;
+
+    // ①：ポインタ変数のラベル
+    printf("pnum1   :%d\n", sizeof(pnum1));
+    printf("pnum2   :%d\n", sizeof(pnum2));
+    printf("pnum3   :%d\n", sizeof(pnum3));
+
+    printf("------------\n");
+
+    // ②：ポインタ変数の型
+    printf("char  * :%d\n", sizeof(char *));
+    printf("short * :%d\n", sizeof(short *));
+    printf("long  * :%d\n", sizeof(long *));
+
+    printf("------------\n");
+
+    // ③：ポインタ変数の参照先
+    printf("*pnum1  :%d\n", sizeof(*pnum1));
+    printf("*pnum2  :%d\n", sizeof(*pnum2));
+    printf("*pnum3  :%d\n", sizeof(*pnum3));
+ 
     return 0;
 }
