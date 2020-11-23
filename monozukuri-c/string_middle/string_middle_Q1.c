12d143d4bfb98696d663de92dbc891f3ded174f1
cl-kn <gitclpg@outlook.jp>
2020-11-23 14:03:46 +0900

    edit string_middle_Q1 (at cm)

--- monozukuri-c/string_middle/string_middle_Q1.c
+++ monozukuri-c/string_middle/string_middle_Q1.c
@@ -7,16 +7,71 @@
  * 引数：
  * 1: char * search_str 検索対象の文字列
  * 2: char count_str カウント対象の文字列
- * 戻り値：int [0以上] 対象文字の数 [-1] 異常終了
+ * 戻り値：int [0以上（正常）] 対象文字の数 [-1] 異常終了
  * 概要：第１引数で指定された文字列の中から、第２引数で指定した文字の数を数える
  */
 //***************************************************
 
 #include <stdio.h>
+#include <string.h>
 
-//
+//自作の関数
+int countCharacter(char *search_str, char count_str)
+{
+    int count = 0;
+
+    //解答より追加のエラーチェック
+    if (search_str == NULL)
+    {
+        return -1;
+    }
+
+    while (*search_str != '\0')
+    {
+        if (*search_str == count_str)
+        {
+            count++;
+        }
+        search_str++;
+    }
+    return count;
+}
+
+//解答の関数
+int countCharacterAnswer(char *search_str, char count_str)
+{
+    int count = 0;
+    int i;
+    if (search_str == NULL)
+    {
+        return -1;
+    }
+
+    //文字列全体を順に参照
+    for (i = 0; i < strlen(search_str); i++)
+    {
+        //一致文字をカウント
+        if (search_str[i] == count_str)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 int main(void)
 {
+    char sentence[] = "I'm studying programming at a manufacturing C language school."; //検索対象の文字列
+    char search_str;                                                                    //検索する文字
+    int count_str;                                                                      //一致した文字数
+
+    search_str = 'a';
+    count_str = countCharacter(sentence, search_str);
+    printf("文字：%c 個数：%d\n", search_str, count_str);
+
+    search_str = 'm';
+    count_str = countCharacter(sentence, search_str);
+    printf("文字：%c 個数：%d\n", search_str, count_str);
+
     return 0;
 }
