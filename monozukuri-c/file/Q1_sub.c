bbf55e261dabbc4c6ec16c465296c8d1a3c918f9
cl-kn <gitclpg@outlook.jp>
2020-11-25 14:31:34 +0900

    add file (at cl)

---
+++ monozukuri-c/file/Q1_sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Q1_sub.h"
+
+void sub_Hello(void)
+{
+    printf("Hello\n");
+}
+
+// int main(void)
+// {
+//     return 0;
+// }
---
+++ monozukuri-c/file/Q1_sub.h
@@ -0,0 +1,10 @@
+#ifndef SUB_H
+#define SUB_H
+
+// #include <stdio.h>
+// #include "Q1_sub.h"
+
+//プロトタイプ宣言
+void sub_Hello(void);
+
+#endif
