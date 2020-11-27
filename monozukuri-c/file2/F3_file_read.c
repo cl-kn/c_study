61c1a5e2867fc39bdbcf17b0805100d4b27e0f93
cl-kn <gitclpg@outlook.jp>
2020-11-27 14:58:19 +0900

    edit (at cl)

---
+++ monozukuri-c/file2/F3_file_read.c
@@ -0,0 +1,52 @@
+//***************************************************
+/**  ファイルから読み込み【fgetc／fgets／fscanfの使い方】
+ * https://monozukuri-c.com/langc-file-read/
+ * 11/27
+*/
+//***************************************************
+
+#include <stdio.h>
+
+int main(void)
+{
+#if 0
+    //fgetc()
+    FILE *fp = fopen("Hello.txt", "r");
+    char c;
+
+    while ((c = fgetc(fp)) != EOF)
+    {
+        printf("%c", c);
+    }
+
+    fclose(fp);
+
+#endif
+
+#if 0
+    //fgets()
+    FILE *fp2 = fopen("num2.txt", "r");
+    char str[256];
+
+    while (fgets(str, 256, fp2) != NULL)
+    {
+        printf("%s", str);
+    }
+
+    fclose(fp2);
+
+#endif
+
+#if 1
+
+    char name[32] = {0};
+    int age = 0;
+    double height = 0;
+    char blood_type = 0;
+
+    FILE *fp3 = fopen("meibo.txt", "r");
+
+#endif
+
+    return 0;
+}
